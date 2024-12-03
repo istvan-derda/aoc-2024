905222e3035447d20d9240719a9d7a1b9d999063
István Derda <mail@istvan-derda.de>
2024-12-03 14:16:41 +0100

    wip 02.2

--- 02/02.2/solve.py
+++ 02/02.2/solve.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 def solve(intext):
     rows: list[str] = [row for row in intext.split("\n") if row != ""]
     reports: list[list[int]]= [[int(level) for level in row.split(" ")] for row in rows]
@@ -9,20 +11,39 @@ def solve(intext):
 
 
 def verify_report(report: list[int]) -> bool:
-    incr_count = 0
-    decr_count = 0
+    print("---")
+    print(report)
+    bad_level = find_bad_level(report)
+    if bad_level == None:
+        print("ok")
+        return True
+    print(bad_level)
+    fix_attempt1 = report[:bad_level] + report[bad_level + 1:]
+    fix_attempt2 = report[:bad_level + 1] + report[bad_level + 2:]
+    print(fix_attempt1)
+    print(fix_attempt2)
+    if find_bad_level(fix_attempt1) == None:
+        print("ok")
+        return True
+    if find_bad_level(fix_attempt2) == None:
+        print("ok")
+        return True
+    print("no")
+    return False
+
+
+def find_bad_level(report: list[int]) -> Optional[int]:
     i = 0
+    is_increasing = None
     while i+1 < len(report):
-        if abs(report[i] - report[i+1]) < 1 or abs(report[i] - report[i+1]) > 3:
-            return False
-        if report[i] < report[i+1]:
-            incr_count += 1
-        else:
-            decr_count += 1
+        if (abs(report[i] - report[i+1]) < 1) or (abs(report[i] - report[i+1]) > 3):
+            return i
+        if is_increasing == None:
+            is_increasing = report[i] < report[i+1]
+        if is_increasing != (report[i] < report[i+1]):
+            return i
         i += 1
-    if (incr_count > 1 and decr_count > 1):
-        return False
-    return True
+    return None
 
 
 def main():
